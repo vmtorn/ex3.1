72aa32a3038c094313ae2fcd4ebc22f3a0a378b8
vmtorn <vtorniko@gmail.com>
2021-12-02 16:06:16 +0200

    testi muutos

--- example-frontend/src/components/NavBar.js
+++ example-frontend/src/components/NavBar.js
@@ -4,6 +4,7 @@ import toskalogo from "../assets/toskalogo.svg";
 const NavBar = () => (
   <div className="navbar">
     <img src={toskalogo} style={{ height: "100%" }} alt="toska" />
+    <p>Terve</p>
   </div>
 );
 
